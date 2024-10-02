082a82d8a4a927cab0ba514d67888e7692ea8e15
arsalanabdulla <71911000+arsalanabdulla@users.noreply.github.com>
2024-10-02 03:19:36 +0300

    Update .app_url.js

--- frontend/src/.app_url.js
+++ frontend/src/.app_url.js
@@ -1,5 +1,5 @@
-const REACT_APP_BASE_USER_URL='http://localhost:3000/api/users'
-const REACT_APP_BASE_POST_URL='http://localhost:3000/api/posts'
-const REACT_APP_ASSETS_URL='http://localhost:3000'
+const REACT_APP_BASE_USER_URL='https://blog-mern-khaki.vercel.app/api/users'
+const REACT_APP_BASE_POST_URL='https://blog-mern-khaki.vercel.app/api/posts'
+const REACT_APP_ASSETS_URL='https://blog-mern-khaki.vercel.app'
 
 export {REACT_APP_BASE_USER_URL,REACT_APP_BASE_POST_URL,REACT_APP_ASSETS_URL}
